635ee8d4eb6cef23007a768470bc6cd7eb20b3f3
ziginsider <ziginsider@gmail.com>
2018-04-26 16:36:35 +0300

    add SaveImageHelper complete

--- app/src/main/java/io/github/ziginsider/epam_laba_12/image/SaveImageHelper.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_12/image/SaveImageHelper.kt
@@ -1,17 +1,35 @@
 package io.github.ziginsider.epam_laba_12.image
 
 import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.provider.MediaStore
 import android.support.v7.app.AlertDialog
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
 import java.lang.ref.WeakReference
 
-class SaveImageHelper(private val alertDialogWeakReference: WeakReference<AlertDialog>,
-                      private val contentResolverWeakReference: WeakReference<ContentResolver>,
-                      private val name: String,
-                      private val description: String): Target {
+class SaveImageHelper(): Target {
+    private lateinit var context: Context
+    private lateinit var alertDialogWeakReference: WeakReference<AlertDialog>
+    private lateinit var contentResolverWeakReference: WeakReference<ContentResolver>
+    private lateinit var name: String
+    private lateinit var description: String
+
+    constructor(context: Context,
+                alertDialog: AlertDialog,
+                contentResolver: ContentResolver,
+                name: String,
+                description: String) : this() {
+        this.context = context
+        alertDialogWeakReference = WeakReference<AlertDialog>(alertDialog)
+        contentResolverWeakReference = WeakReference<ContentResolver>(contentResolver)
+        this.name = name
+        this.description = description
+    }
+
     override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
 
     }
@@ -20,6 +38,20 @@ class SaveImageHelper(private val alertDialogWeakReference: WeakReference<AlertD
     }
 
     override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+        val resolver = contentResolverWeakReference.get()
+        val dialog =  alertDialogWeakReference.get()
+
+        if (resolver != null) {
+            MediaStore.Images.Media.insertImage(resolver, bitmap, name, description)
+        }
+        dialog?.dismiss()
+
+        val intent = Intent()
+        with(intent) {
+            setType("image/*")
+            setAction(Intent.ACTION_GET_CONTENT)
+        }
+        context.startActivity(Intent.createChooser(intent, "View image"))
     }
 
 }
