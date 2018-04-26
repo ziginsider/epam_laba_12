7b9139eabb31ddb1634a669ca43f4402c35fe575
ziginsider <ziginsider@gmail.com>
2018-04-26 22:59:11 +0300

    add BoundService

---
+++ app/src/main/java/io/github/ziginsider/epam_laba_12/BoundService.kt
@@ -0,0 +1,24 @@
+package io.github.ziginsider.epam_laba_12
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+
+class BoundService: Service() {
+    interface ServiceImageLoadingListener {
+        fun onImageLoadingDone(url: String)
+    }
+    private val myBinder = MyBinder()
+
+    override fun onBind(intent: Intent?): IBinder {
+        val str = intent?.extras?.get("URL")
+        return myBinder
+    }
+
+    inner class MyBinder: Binder() {
+        fun getService() : BoundService {
+            return this@BoundService
+        }
+    }
+}
