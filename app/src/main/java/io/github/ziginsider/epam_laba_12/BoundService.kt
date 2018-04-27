d7e51aea06012a29b139921b5e2c75a346eb1c9f
ziginsider <ziginsider@gmail.com>
2018-04-27 20:36:26 +0300

    BoundService callback to Activity work version

--- app/src/main/java/io/github/ziginsider/epam_laba_12/BoundService.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_12/BoundService.kt
@@ -4,12 +4,18 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import com.squareup.picasso.Picasso
+import io.github.ziginsider.epam_laba_12.image.SaveImageHelper
+import java.util.*
 
-class BoundService: Service() {
+class BoundService: Service(), SaveImageHelper.ImageLoadingListener {
     interface ServiceImageLoadingListener {
-        fun onImageLoadingDone(url: String)
+        fun onImageLoadingDone(url: String?)
     }
+
     private val myBinder = MyBinder()
+    private var listener: ServiceImageLoadingListener? = null
 
     override fun onBind(intent: Intent?): IBinder {
         val str = intent?.extras?.get("URL")
@@ -17,19 +23,35 @@ class BoundService: Service() {
     }
 
     inner class MyBinder: Binder() {
+
         fun getService() : BoundService {
             return this@BoundService
+            Log.d("TAG", "getService")
         }
     }
-
     fun doImageLoading(url: String, listener: ServiceImageLoadingListener) {
-        Thread(Runnable {
-            val storageUrl = imageLoading(url)
-            listener.onImageLoadingDone(storageUrl)
-        })
+        this.listener = listener
+        //Thread(Runnable {
+            imageLoading(url)
+        //}).start()
     }
 
     private fun imageLoading(url: String): String {
+
+        val fileName = UUID.randomUUID().toString() + ".jpg"
+        Picasso.with(this)
+                .load("https://us.123rf.com/450wm/mondaian/mondaian1701/mondaian170100117/71437596-roman-coliseum.jpg")
+                //.resize(2000, 1000)
+                .into(SaveImageHelper(
+                        this,
+                        applicationContext.contentResolver,
+                        fileName,
+                        "image description"))
+
         return ""
+    }
+
+    override fun imageLoaded(url: String?) {
+        listener?.onImageLoadingDone(url)
     }
 }
--- app/src/main/java/io/github/ziginsider/epam_laba_12/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_12/MainActivity.kt
@@ -2,9 +2,14 @@ package io.github.ziginsider.epam_laba_12
 
 import android.Manifest
 import android.app.ProgressDialog
+import android.app.Service
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
@@ -13,15 +18,23 @@ import io.github.ziginsider.epam_laba_12.image.SaveImageHelper
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.indeterminateProgressDialog
 import org.jetbrains.anko.toast
+import java.lang.ref.WeakReference
 import java.util.*
 
-class MainActivity : AppCompatActivity(), SaveImageHelper.ImageLoadingListener {
+class MainActivity : AppCompatActivity(), BoundService.ServiceImageLoadingListener {
     private val REQUEST_PERMISSION_CODE = 1
     private lateinit var progressDialog: ProgressDialog
+    var boundService: BoundService? = null
+    var isBound = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindService(Intent(this, BoundService::class.java),
+                myConnection,
+                Service.BIND_AUTO_CREATE)
+        isBound = true
 
         downloadButton.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -34,15 +47,27 @@ class MainActivity : AppCompatActivity(), SaveImageHelper.ImageLoadingListener {
                 progressDialog = indeterminateProgressDialog("Loading", "Image loading...")
                 progressDialog.show()
 
-                val fileName = UUID.randomUUID().toString() + ".jpg"
-                Picasso.with(this)
-                        .load("https://us.123rf.com/450wm/mondaian/mondaian1701/mondaian170100117/71437596-roman-coliseum.jpg")
-                        //.resize(2000, 1000)
-                        .into(SaveImageHelper(
-                                this,
-                                applicationContext.contentResolver,
-                                fileName,
-                                "image description"))
+//                val fileName = UUID.randomUUID().toString() + ".jpg"
+//                Picasso.with(this)
+//                        .load("https://us.123rf.com/450wm/mondaian/mondaian1701/mondaian170100117/71437596-roman-coliseum.jpg")
+//                        //.resize(2000, 1000)
+//                        .into(SaveImageHelper(
+//                                this,
+//                                applicationContext.contentResolver,
+//                                fileName,
+//                                "image description"))
+
+                boundService?.let {
+                    it.doImageLoading("https://us.123rf.com/450wm/mondaian/mondaian1701/mondaian170100117/71437596-roman-coliseum.jpg",
+                            BoundServiceListener(this))
+                }
+
+                //Bound Service
+                Log.d("TAG", "downloadPress")
+//                bindService(Intent(this@MainActivity, BoundService::class.java),
+//                        myConnection,
+//                        Service.BIND_AUTO_CREATE)
+//                isBound = true
             }
         }
     }
@@ -61,10 +86,60 @@ class MainActivity : AppCompatActivity(), SaveImageHelper.ImageLoadingListener {
         }
     }
 
-    override fun imageLoaded(url: String?) {
-        if (progressDialog.isShowing) {
-            progressDialog.dismiss()
+//    override fun imageLoaded(url: String?) {
+//        if (progressDialog.isShowing) {
+//            progressDialog.dismiss()
+//        }
+//        Log.d("TAG", "url = $url")
+//    }
+
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName?, service: IBinder?) {
+            val binder = service as BoundService.MyBinder
+            boundService = binder.getService()
+            //boundService?.doImageLoading("https://us.123rf.com/450wm/mondaian/mondaian1701/mondaian170100117/71437596-roman-coliseum.jpg",
+            //        BoundServiceListener(this@MainActivity))
+            Log.d("TAG", "onServiceConnected")
         }
-        Log.d("TAG", "url = $url")
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            boundService = null
+            Log.d("TAG", "onServiceDisconnected")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isBound) {
+            try {
+                unbindService(myConnection)
+                isBound = false
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onImageLoadingDone(url: String?) {
+
+    }
+
+    private class BoundServiceListener(activity: MainActivity)
+        : BoundService.ServiceImageLoadingListener {
+        private val weakActivity: WeakReference<MainActivity>
+                = WeakReference<MainActivity>(activity)
+
+        override fun onImageLoadingDone(url: String?) {
+            val localReferenceActivity = weakActivity.get()
+            localReferenceActivity?.let {
+                with(it) {
+                    runOnUiThread({
+                        if (progressDialog.isShowing) {
+                            progressDialog.dismiss()
+                        }
+                    })
+                }
+            }
+        }
     }
 }
