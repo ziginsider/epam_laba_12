c00656da85aaf2ac8d9a5a9292d7871fef0f092a
ziginsider <ziginsider@gmail.com>
2018-05-01 13:48:47 +0300

    add ScheduledServices complete

--- app/src/main/java/io/github/ziginsider/epam_laba_12/JobSchedulerService.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_12/JobSchedulerService.kt
@@ -4,11 +4,15 @@ import android.annotation.TargetApi
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.os.Build
+import io.github.ziginsider.epam_laba_12.download.DOWNLOADED_FILE_NAME
+import io.github.ziginsider.epam_laba_12.download.RETROFIT_BASE_URL
+import io.github.ziginsider.epam_laba_12.download.RETROFIT_GET_REQUEST
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class JobSchedulerService: JobService() {
     override fun onStartJob(params: JobParameters?): Boolean {
-
+        ScheduledService.startScheduledJob(applicationContext, RETROFIT_BASE_URL,
+                RETROFIT_GET_REQUEST, DOWNLOADED_FILE_NAME)
         return false
     }
 
--- app/src/main/java/io/github/ziginsider/epam_laba_12/ScheduledService.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_12/ScheduledService.kt
@@ -7,8 +7,7 @@ import android.content.Intent
 import android.os.Environment
 import android.support.v4.app.NotificationCompat
 import android.support.v4.content.LocalBroadcastManager
-import io.github.ziginsider.epam_laba_12.download.Download
-import io.github.ziginsider.epam_laba_12.download.RetrofitService
+import io.github.ziginsider.epam_laba_12.download.*
 import okhttp3.ResponseBody
 import retrofit2.Retrofit
 import java.io.BufferedInputStream
@@ -16,23 +15,43 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
-class ScheduledService(name: String?) : IntentService(name) {
+const val ACTION = "sheduled_action"
+
+class ScheduledService(name: String? = "Scheduled Service") : IntentService(name) {
     private var notificationBuilder: NotificationCompat.Builder? = null
     private var notificationManager: NotificationManager? = null
     private var totalMBFileSize: Int = 0
     private lateinit var filePath: File
 
+    companion object {
+        fun startScheduledJob(context: Context, urlBase: String, urlFile: String,
+                              nameDownloadedFile: String) {
+            val intent = Intent(context, ScheduledService::class.java)
+            intent.action = ACTION
+            intent.putExtra(KEY_BASE_URL, urlBase)
+            intent.putExtra(KEY_GET_REQUEST, urlFile)
+            intent.putExtra(KEY_FILE_NAME, nameDownloadedFile)
+            context.startService(intent)
+        }
+    }
 
     override fun onHandleIntent(intent: Intent?) {
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
-                as NotificationManager?
-        notificationBuilder = NotificationCompat.Builder(this, "ch1")
-                .setSmallIcon(R.drawable.ic_file_download)
-                .setContentTitle("Download")
-                .setContentText("Downloading file")
-                .setAutoCancel(true)
-        notificationManager?.notify(0, notificationBuilder?.build())
-        initDownload(urlBase, urlFile, nameDownloadedFile)
+        intent?.let {
+            if (it.action == ACTION) {
+                val urlBase = it.getStringExtra(KEY_BASE_URL)
+                val urlFile = it.getStringExtra(KEY_GET_REQUEST)
+                val nameDownloadedFile = it.getStringExtra(KEY_FILE_NAME)
+                notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                        as NotificationManager?
+                notificationBuilder = NotificationCompat.Builder(this, "ch1")
+                        .setSmallIcon(R.drawable.ic_file_download)
+                        .setContentTitle("Download")
+                        .setContentText("Downloading file")
+                        .setAutoCancel(true)
+                notificationManager?.notify(0, notificationBuilder?.build())
+                initDownload(urlBase, urlFile, nameDownloadedFile)
+            }
+        }
     }
 
     private fun initDownload(urlBase: String, urlFile: String, nameDownloadedFile: String) {
